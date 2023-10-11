bd56e28fce7e01bdab10c7225a94fa6c4f0db25b
Guilherme <guilherme.teixeira013@protonmail.com>
2023-10-11 10:27:07 -0300

    property name with number

--- random testings/JS-Objects2/index.js
+++ random testings/JS-Objects2/index.js
@@ -8,4 +8,15 @@ let school = {
     }
 }
 
-school.displayInfo();
+school.displayInfo(); //displayInfo() is a method inside the School object
+
+// Accessing property name with number;
+
+let exemple = {
+    14 : "test",
+    2 : "another text",
+    name : "guilherme",
+}
+console.log(exemple[14]);       // its like using array
+console.log(exemple[2]);
+console.log(exemple.name);
