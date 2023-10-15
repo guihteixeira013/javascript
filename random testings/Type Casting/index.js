cbd20ff1eadff5db72f764a792e1f8859323655e
Guilherme <guilherme.teixeira013@protonmail.com>
2023-10-15 15:38:39 -0300

    Casting between BOOLEAN and STRING.

--- random testings/Type Casting/index.js
+++ random testings/Type Casting/index.js
@@ -1,20 +1,15 @@
 // TYPE CASTING (fundição de tipo)
 
-// Casting NUMBER to STRING
-    // here is the variable with a number  
-let num = 1;
-console.log(`${num} type: ${typeof(num)}`);
+// // BOOLEAN to STRING
+// let value = true;
+// console.log(typeof(value));
+//     // the casting moment:
+// value = String(value);
+// console.log(typeof(value));
 
-    // casting moment
-num = String(num);
-console.log(`${num} type: ${typeof(num)}`);
-
-
-// Casting STRING to NUMBER
-    // here you can see a variable with a string
-let text = "1.3"; 
-console.log(`${text} type: ${typeof(text)}`);
-
-    // the casting moment
-text = Number(text);
-console.log(`${text} type: ${typeof(text)}`);
+// STRING to BOOLEAN
+let value = "true";
+console.log(typeof(value));
+    // the casting moment:
+value = Boolean(value);
+console.log(typeof(value));
